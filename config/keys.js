38f7a7a7a9d8408726adca4a4e02d9db60451d6d
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-03-17 09:34:54 +0300

    passport.js refactoring

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientId: '945839613012-s2l5g2rb3afumkiae4lbmqnhs25uh36j.apps.googleusercontent.com',
+    clientSecret: 'GVBrHElrlhJr7_teEEz09q7L'
+}
--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStartegy = require('passport-google-oauth20').Strategy
+require('./services/passport')
 
-const keys = require('./config/keys')
-
-
-const PORT = process.env.PORT || 5000
 
 const app = express()
 
-passport.use(new GoogleStartegy({
-    clientID: keys.googleClientId,
-    clientSecret: keys.clientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken)
-    console.log('refreshToken', refreshToken)
-    console.log('profile', profile)
-    }
-    ))
+require('./routes/authRoutes')(app)
 
-
-
-app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
 
