b2c03b231bb29717d16a6120bc6160947a5b1e4b
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-03-18 23:44:31 +0300

    finish auth with google

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require('passport')
 const GoogleStartegy = require('passport-google-oauth20').Strategy
 
+const mongoose = require('mongoose')
+
+User = mongoose.model('users')
+
 const keys = require('../config/keys')
 
 passport.use(new GoogleStartegy({
@@ -11,5 +15,19 @@ passport.use(new GoogleStartegy({
         console.log('accessToken', accessToken)
         console.log('refreshToken', refreshToken)
         console.log('profile', profile)
+
+        User.findOne({googleId: profile.id})
+            .then(existingUser =>{
+                if(existingUser){
+                    done(null, existingUser)
+                }
+                else{
+                    new User({ googleId: profile.id})
+                        .save()
+                        .then(user => done(null, user))
+                }
+            })
+
+
     }
 ))
